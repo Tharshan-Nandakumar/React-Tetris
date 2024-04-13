849fbb6dd9ac50b19df5f982f61be7a9752b7b15
Tharshan <tharshan1465@gmail.com>
2024-04-13 04:53:41 +0100

    cell

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -2,8 +2,9 @@ import React from "react";
 import { StyledCell } from "./styles/StyledCell";
 import { TETROMINOS } from "../tetrominos";
 
-const Cell = ({ type }) => {
-  return <StyledCell type={type} color={TETROMINOS[type].color} />;
-};
+const Cell = ({ type }) => (
+  <StyledCell type={type} color={TETROMINOS[type].color} />
+);
 
-export default Cell;
+// React.memo only re-renders cells which change
+export default React.memo(Cell);
